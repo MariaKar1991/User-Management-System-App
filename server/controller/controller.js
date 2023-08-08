c36729a85e9e4f2520e4e8b1ea9a055232dadbd8
MariaKar1991 <mariaskaravazaki@gmail.com>
2023-08-08 18:36:01 +0300

    Rename master to main

---
+++ server/controller/controller.js
@@ -0,0 +1,13 @@
+var Userdb = require("../model/model");
+
+// create and save new user
+exports.create = (req, res) => {};
+
+// retrieve and return all users/ retrive and return a single user
+exports.find = (req, res) => {};
+
+// Update a new identifier user by user id
+exports.update = (req, res) => {};
+
+// Delete a user with specified user id in the request
+exports.delete = (req, res) => {};
