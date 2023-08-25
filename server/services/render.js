850f804462575d0215f69c3e2ef6a4fcd50f6b28
Maria Karavazaki <mariaskaravazaki@gmail.com>
2023-08-25 10:56:24 +0300

    Update render.js

--- server/services/render.js
+++ server/services/render.js
@@ -1,11 +1,17 @@
 const axios = require("axios");
 const { Query } = require("mongoose");
 
+/**
+ * @description Renders the home page with a list of users
+ * @param {object} req - Express request object
+ * @param {object} res - Express response object
+ */
 exports.homeRoutes = (req, res) => {
-  // Make a get request to /api/users
+  // Make a GET request to retrieve user data from the API
   axios
     .get("http://localhost:4002/api/users")
     .then(function (response) {
+      // Render the "index" view with user data
       res.render("index", { users: response.data });
     })
     .catch((err) => {
@@ -13,19 +19,33 @@ exports.homeRoutes = (req, res) => {
     });
 };
 
+/**
+ * @description Renders the "Add User" page
+ * @param {object} req - Express request object
+ * @param {object} res - Express response object
+ */
 exports.add_user = (req, res) => {
+  // Render the "add_user" view
   res.render("add_user");
 };
 
+/**
+ * @description Renders the "Update User" page with user data for a specific user
+ * @param {object} req - Express request object
+ * @param {object} res - Express response object
+ */
 exports.update_user = (req, res) => {
+  // Make a GET request to retrieve specific user data from the API
   axios
     .get("http://localhost:4002/api/users", {
       params: { id: req.query.id },
     })
     .then(function (userdata) {
+      // Render the "update_user" view with the user data
       res.render("update_user", { user: userdata.data });
     })
     .catch((err) => {
       res.send(err);
     });
 };
+
