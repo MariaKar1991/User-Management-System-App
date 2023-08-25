d631e4d84bd05d601590ba4f0081641614ae8bc7
Maria Karavazaki <mariaskaravazaki@gmail.com>
2023-08-25 11:15:44 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,41 +1,45 @@
+// Import required modules and packages
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const bodyparser = require("body-parser");
 const path = require("path");
 
+// Import the function to connect to the database
 const connectDB = require("./server/database/connection");
 
+// Create an Express application instance
 const app = express();
 
+// Load environment variables from the "config.env" file
 dotenv.config({ path: "config.env" });
+
+// Set the port number for the server to listen on
 const PORT = process.env.PORT || 4000;
 
-//log requests
+// Log requests using the morgan middleware
 app.use(morgan("tiny"));
 
-// mongoDB connection
+// Establish a connection to the MongoDB database
 connectDB();
 
-//parse request to body-parser
+// Parse incoming requests with URL-encoded data using body-parser
 app.use(bodyparser.urlencoded({ extended: true }));
 
-//set view engine
+// Set the view engine for rendering dynamic content (using EJS)
 app.set("view engine", "ejs");
 //app.set("views".path.resolve(__dirname, "views/ejs"));
 
-// load assets
+// Serve static assets (CSS, images, JavaScript)
 app.use("/css", express.static(path.resolve(__dirname, "assets/css")));
 app.use("/img", express.static(path.resolve(__dirname, "assets/img")));
 app.use("/js", express.static(path.resolve(__dirname, "assets/js")));
 
-// load routers
+// Load and use the defined routers for handling routes
 app.use("/", require("./server/routes/router"));
 
-//app.get("/", (req, res) => {
-//  res.send("Hello Maria");
-//});
-
+// Start the server and listen for incoming connections
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
