040afced5e9f171881cc6e7d0f06945b94a83448
Maria Karavazaki <mariaskaravazaki@gmail.com>
2023-08-25 10:53:40 +0300

    Update model.js

--- server/model/model.js
+++ server/model/model.js
@@ -1,19 +1,23 @@
 const mongoose = require("mongoose");
 
+// Define a schema for the User collection
 var schema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
+    required: true, // The 'name' field is required for every user
   },
   email: {
     type: String,
-    required: true,
-    unique: true,
+    required: true, // The 'email' field is required for every user
+    unique: true,   // The 'email' field must be unique
   },
-  gender: String,
-  status: String,
+  gender: String,   // The 'gender' field is optional
+  status: String,   // The 'status' field is optional
 });
 
+// Create a User model using the defined schema
 const Userdb = mongoose.model("userdb", schema);
 
+// Export the Userdb model to be used in other parts of the application
 module.exports = Userdb;
+
