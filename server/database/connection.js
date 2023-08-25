e255ca296e40d70b5781ae03de2487be60142fe5
Maria Karavazaki <mariaskaravazaki@gmail.com>
2023-08-25 10:52:47 +0300

    Update connection.js

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,18 +1,25 @@
 const mongoose = require("mongoose");
 
+/**
+ * Asynchronous function to establish a connection with the MongoDB database.
+ * @returns {Promise<void>} A Promise that resolves when the connection is established or rejects on error.
+ */
 const connectDB = async () => {
   try {
-    // mongodb connection string
+    // Establish a connection to the MongoDB database using the provided connection string
     const con = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
 
-    console.log(`MongoDB connected:${con.connection.host}`);
+    // Log a success message upon successful database connection
+    console.log(`MongoDB connected: ${con.connection.host}`);
   } catch (err) {
-    console.log(err);
+    // Log the error and exit the process with an error code if connection fails
+    console.error(err);
     process.exit(1);
   }
 };
 
+// Export the connectDB function to be used in other parts of the application
 module.exports = connectDB;
